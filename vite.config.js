20d3e3b544d404d8b1815b7e1bc12b12c48ed8a4
Wian <wianv@fraxion.biz>
2025-03-27 13:20:54 +0200

    Setup GitHub Pages deployment with React Router support

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Budget-app/',
   plugins: [
     react(),
     VitePWA({
@@ -17,11 +19,6 @@ export default defineConfig({
           {
             src: 'pwa-192x192.png',
             sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
             type: 'image/png'
           }
         ]
